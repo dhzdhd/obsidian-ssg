4263d04be45fabdfd360ff8a035a044d65a8787a
dhzdhd <ragas5432100@gmail.com>
2024-01-15 20:52:44 +0530

    Make TOC and Tree responsive

--- src/components/TOC.svelte
+++ src/components/TOC.svelte
@@ -1,19 +1,40 @@
 <script lang="ts">
   import type { MarkDownHeading } from "astro";
+  import { PanelRight } from "lucide-svelte";
+  import { fly } from "svelte/transition";
 
   export let data: MarkDownHeading;
+  let innerWidth;
+  let visible = false;
+
+  $: visible = innerWidth > 1024 ? true : false;
 </script>
 
 <!-- TODO: Move TOC to vanilla Astro -->
 
-<h1 class="pb-3 pl-2">On this page</h1>
-<ul>
-  {#each data as item}
-    <li
-      style={`padding-left: ${(item.depth - 2) * 1.2 + 0.5}rem`}
-      class="py-1 hover:bg-secondary dark:hover:bg-primary-foreground w-full"
-    >
-      <a class="w-full inline-flex" href={`#${item.slug}`}>{item.text}</a>
-    </li>
-  {/each}
-</ul>
+<svelte:window bind:innerWidth />
+
+{#if visible}
+  <aside
+    transition:fly={{ x: 200 }}
+    class="fixed right-0 top-14 w-80 border-l-[1px] h-svh p-5 bg-primary-foreground dark:bg-background"
+  >
+    <nav>
+      <h1 class="pb-3 pl-2">On this page</h1>
+      <ul>
+        {#each data as item}
+          <li
+            style={`padding-left: ${(item.depth - 2) * 1.2 + 0.5}rem`}
+            class="py-1 hover:bg-secondary dark:hover:bg-primary-foreground w-full"
+          >
+            <a class="w-full inline-flex" href={`#${item.slug}`}>{item.text}</a>
+          </li>
+        {/each}
+      </ul>
+    </nav>
+  </aside>
+{/if}
+<button
+  on:click={() => (visible = !visible)}
+  class="fixed bottom-2 right-2 opacity-70"><PanelRight /></button
+>
--- src/components/Tree.svelte
+++ src/components/Tree.svelte
@@ -3,6 +3,8 @@
   import { type TreeItem } from "@components/ui/tree.svelte";
   import { setContext } from "svelte";
   import { createTreeView } from "@melt-ui/svelte";
+  import { PanelLeft } from "lucide-svelte";
+  import { fly } from "svelte/transition";
 
   const ctx = createTreeView({
     defaultExpanded: ["lib-0", "tree-0"],
@@ -14,11 +16,25 @@
   } = ctx;
 
   export let treeItems: TreeItem[];
+
+  let innerWidth;
+  let visible = false;
+
+  $: visible = innerWidth > 1024 ? true : false;
 </script>
 
-<ul
-  class="flex-shrink-0 fixed w-80 h-svh top-14 border-r-[1px] py-5 px-3 list-none"
-  {...$tree}
+<svelte:window bind:innerWidth />
+
+{#if visible}
+  <ul
+    transition:fly={{ x: -200 }}
+    class="flex-shrink-0 fixed w-80 h-svh top-14 border-r-[1px] py-5 px-3 list-none bg-primary-foreground dark:bg-background"
+    {...$tree}
+  >
+    <Tree {treeItems} />
+  </ul>
+{/if}
+<button
+  on:click={() => (visible = !visible)}
+  class="fixed bottom-2 left-2 opacity-70"><PanelLeft /></button
 >
-  <Tree {treeItems} />
-</ul>
