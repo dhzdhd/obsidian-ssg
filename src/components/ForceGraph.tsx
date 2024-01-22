72406080d3ad971ba70878a68be2e8afa24c388a
dhzdhd <ragas5432100@gmail.com>
2024-01-22 22:01:33 +0530

    Improve graph links

--- src/components/ForceGraph.tsx
+++ src/components/ForceGraph.tsx
@@ -1,6 +1,6 @@
 import { Waypoints } from "lucide-react";
 import { Button } from "@components/ui/button";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -13,24 +13,35 @@ import ForceGraph2D from "react-force-graph-2d";
 import type { GraphData } from "react-force-graph-2d";
 
 const ForceGraph = () => {
+  const fgRef = useRef();
   const [data, setData] = useState<GraphData<any, any>>(
     [] as unknown as GraphData<any, any>
   );
+  const [highlightNodes, setHighlightNodes] = useState(new Set());
+  const [highlightLinks, setHighlightLinks] = useState(new Set());
+  const [hoverNode, setHoverNode] = useState(null);
   useEffect(() => {
-    const raw = {
-      nodes: [
-        { id: "Myriel", group: 1 },
-        { id: "Napoleon", group: 1 },
-        { id: "Mlle.Baptistine", group: 1 },
-      ],
-      links: [
-        { source: "Napoleon", target: "Myriel", value: 1 },
-        { source: "Mlle.Baptistine", target: "Myriel", value: 1 },
-      ],
-    };
-
-    setData(() => raw);
+    setData(() => {
+      return {
+        nodes: [
+          { id: "Myriel", group: 1 },
+          { id: "Napoleon", group: 1 },
+          { id: "Mlle.Baptistine", group: 1 },
+          { id: "Mme.Magloire", group: 1 },
+        ],
+        links: [
+          { source: "Napoleon", target: "Myriel", value: 1 },
+          { source: "Mlle.Baptistine", target: "Myriel", value: 8 },
+          { source: "Mme.Magloire", target: "Myriel", value: 10 },
+        ],
+      };
+    });
   }, []);
+
+  const updateHighlight = () => {
+    setHighlightNodes(highlightNodes);
+    setHighlightLinks(highlightLinks);
+  };
 
   return (
     <>
@@ -46,9 +57,51 @@ const ForceGraph = () => {
             <DialogTitle>Graph view</DialogTitle>
           </DialogHeader>
           <ForceGraph2D
+            ref={fgRef}
             graphData={data}
+            width={500}
+            height={300}
+            // onEngineStop={() => fgRef.current.zoomToFit(1000)}
+            cooldownTicks={100}
             nodeAutoColorBy="group"
-            linkColor="white"
+            linkCanvasObject={(link, ctx, globalScale) => {
+              ctx.beginPath();
+
+              ctx.strokeStyle = highlightLinks.has(link) ? "white" : "grey";
+              ctx.lineWidth = 1 / globalScale;
+              ctx.moveTo(link.source.x, link.source.y);
+              ctx.lineTo(link.target.x, link.target.y);
+              ctx.stroke();
+            }}
+            linkWidth={(link) =>
+              highlightLinks.has(link) ? link.lineWidth * 2 : link.lineWidth
+            }
+            onLinkHover={(link) => {
+              highlightNodes.clear();
+              highlightLinks.clear();
+
+              if (link) {
+                highlightLinks.add(link);
+                highlightNodes.add(link.source);
+                highlightNodes.add(link.target);
+              }
+
+              updateHighlight();
+            }}
+            onNodeHover={(node) => {
+              highlightNodes.clear();
+              highlightLinks.clear();
+              if (node) {
+                highlightNodes.add(node);
+                node.neighbors.forEach((neighbor) =>
+                  highlightNodes.add(neighbor)
+                );
+                node.links.forEach((link) => highlightLinks.add(link));
+              }
+
+              setHoverNode(node || null);
+              updateHighlight();
+            }}
             nodeCanvasObject={(node, ctx, globalScale) => {
               const label = String(node.id);
               const fontSize = 12 / globalScale;
@@ -58,7 +111,7 @@ const ForceGraph = () => {
                 (n) => n + fontSize * 0.2
               );
 
-              ctx.fillStyle = "rgba(0, 0, 0, 0.8)";
+              ctx.fillStyle = "rgba(0, 0, 0, 0.0)";
               ctx.fillRect(
                 node.x - bckgDimensions[0] / 2,
                 node.y - bckgDimensions[1] / 2,
