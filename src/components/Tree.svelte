0b04ed1ef35feb526ed90d585468f8a23b164134
dhzdhd <ragas5432100@gmail.com>
2024-06-20 01:16:34 +0530

    Add overflow scroll to tree

--- src/components/Tree.svelte
+++ src/components/Tree.svelte
@@ -30,7 +30,7 @@
 {#if $visible}
   <ul
     transition:fly={{ x: -200 }}
-    class="z-10 flex-shrink-0 fixed w-80 h-svh top-14 border-r-[1px] py-5 px-3 list-none bg-primary-foreground dark:bg-background"
+    class="z-10 flex-shrink-0 fixed w-80 h-svh top-14 border-r-[1px] overflow-y-auto py-5 px-3 list-none bg-primary-foreground dark:bg-background"
     {...$tree}
   >
     <Tree {treeItems} />
